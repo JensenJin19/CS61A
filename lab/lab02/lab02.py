4992c56995c645f17ba4e8261f06c4bbad206c96
Jensen Jin <jensenjin19@outlook.com>
2025-12-04 00:40:36 +0800

    update: update lab02 and disc02

--- lab/lab02/lab02.py
+++ lab/lab02/lab02.py
@@ -118,5 +118,18 @@ def cycle(f1, f2, f3):
     >>> do_two_cycles(1)
     19
     """
-    "*** YOUR CODE HERE ***"
+    def g(n):
+        def h(x):
+            i = 0
+            while i < n:
+                if i % 3 == 0:
+                    x = f1(x)
+                elif i % 3 == 1:
+                    x = f2(x)
+                elif i % 3 == 2:
+                    x = f3(x)
+                i += 1
+            return x
+        return h
+    return g
 
